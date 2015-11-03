4e3a8e6aee60d5578c23e79f2fd41c76509f86a1
Jack Wang <jack.wang@omniquestinternational.com>
2015-11-03 16:04:24 +0800

    Add carthage support

---
+++ Example/TSMessages-ios/TSMessages-ios.h
@@ -0,0 +1,19 @@
+//
+//  TSMessages-ios.h
+//  TSMessages-ios
+//
+//  Created by Jack Wang on 11/3/15.
+//  Copyright © 2015 Felix Krause. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TSMessages-ios.
+FOUNDATION_EXPORT double TSMessages_iosVersionNumber;
+
+//! Project version string for TSMessages-ios.
+FOUNDATION_EXPORT const unsigned char TSMessages_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TSMessages_ios/PublicHeader.h>
+
+
