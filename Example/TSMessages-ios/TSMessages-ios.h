80ea656ddecf03b013eb88b4de7f89d9b6791ea7
Jack Wang <jack.wang@omniquestinternational.com>
2015-11-05 11:45:36 +0800

    Revert "update framework name"
    This reverts commit b046daf8bfcfdd3427103bbb71d0f8e5acaf9e5c.

--- Example/TSMessages-ios/TSMessages-ios.h
+++ Example/TSMessages-ios/TSMessages-ios.h
@@ -17,7 +17,3 @@ FOUNDATION_EXPORT const unsigned char TSMessages_iosVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <TSMessages_ios/PublicHeader.h>
 
 
-
-#import "TSMessage.h"
-#import "TSBlurView.h"
-#import "TSMessageView.h"
