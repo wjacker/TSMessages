48297004d793a348151196cf7e1a4b22f1ab0af1
Jack Wang <wjacker2@gmail.com>
2015-11-18 15:42:46 +0800

    Update TSMessage framework

---
+++ Pod/Classes/TSMessage_Header.h
@@ -0,0 +1,16 @@
+//
+//  TSMessage_Header.h
+//  Pods
+//
+//  Created by Jack Wang on 11/18/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "TSMessage.h"
+#import "TSMessageView.h"
+#import "TSBlurView.h"
+
+@interface TSMessage(Header)
+
+@end
